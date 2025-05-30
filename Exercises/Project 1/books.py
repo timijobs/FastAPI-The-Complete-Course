92684c64345d8d1cb301cd83e66e0ab7c2ec26cb
unknown <timilehineinstein@gmail.com>
2025-05-30 16:23:26 +0100

    added dependencies from requirements.txt

---
+++ Exercises/Project 1/books.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/api/books")
+async def read_all_books():
+    return BOOKS
+
+@app.get("/api/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
